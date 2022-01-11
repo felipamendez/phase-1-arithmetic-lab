360a6b8b2eda5d530fdc265c0e3811024cd32254
Felipa Mendez <felipalouise@gmail.com>
2022-01-10 20:53:13 -0600

    commit tree a5ebaf6a59dca04da0f734c6a498dcbbf69cd0d2

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+random;
+
+const num3 = 12;
+const num4 = 3;
+const mod = num3 / num4;
+
+const max = Math.max(1, 2, 3, 4, 5, 20);
